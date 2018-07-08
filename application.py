d0c026ee9db4ee0bba6518d140b493c2c1650e9e
John B. McLellan <jmclellan@hbs.edu>
2018-07-08 17:20:17 +0000

    initial templates

--- application.py
+++ application.py
@@ -1,15 +1,19 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+from passlib.hash import pbkdf2_sha256
 
 app = Flask(__name__)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
+if not os.getenv("DARKSKY_KEY"):
+    raise RuntimeError("DARKSKY_KEY is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -23,4 +27,4 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
